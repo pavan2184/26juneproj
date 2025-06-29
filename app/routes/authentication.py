8b5960973cb58b1ed52f093b29642832da4abe61
Abhijit Jayaraman <abhijit@Abhijits-MacBook-Pro.local>
2025-06-29 13:47:59 +0800

    added authentication and jwt token

---
+++ app/routes/authentication.py
@@ -0,0 +1,47 @@
+from fastapi import APIRouter, HTTPException
+from models import UserRegister, hash_password, verify_password
+from database import users_collection
+from jwttoken import create_access_token
+from pydantic import BaseModel
+
+router = APIRouter()
+
+class LoginModel(BaseModel):
+    email: str
+    password: str
+
+@router.post("/register")
+def register(user: UserRegister):
+    if users_collection.find_one({"email": user.email}):     #linked to where database is
+        return {"msg": "User exists"}
+    
+    user_data = {
+        "email": user.email,
+        "password": hash_password(user.password)  
+    }
+    users_collection.insert_one(user_data)                  #linked to where database is
+    return {"msg": "User registered successfully"}
+
+
+@router.post("/register")
+def register(user: UserRegister):
+    if users_collection.find_one({"email": user.email}):
+        raise HTTPException(status_code=400, detail="User already exists")
+
+    user_data = {
+        "name": user.name,
+        "age": user.age,
+        "email": user.email,
+        "password": hash_password(user.password)
+    }
+    users_collection.insert_one(user_data)
+    return {"msg": "User registered successfully"}
+
+@router.post("/login")
+def login(user: LoginModel):
+    db_user = users_collection.find_one({"email": user.email})
+    if not db_user or not verify_password(user.password, db_user["password"]):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    token = create_access_token(data={"sub": user.email})
+    return {"access_token": token, "token_type": "bearer"}
