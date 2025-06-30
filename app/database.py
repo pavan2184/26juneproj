8880b5802548421bf3b3d79f3503b12b1dbe955b
Abhijit Jayaraman <abhijit@Abhijits-MacBook-Pro.local>
2025-06-30 11:54:44 +0800

    changes

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 # MongoDB connection URI (local default)
-MONGO_URI = "mongodb://localhost:27017"
+MONGO_URI = "mongodb+srv://admin:abhi2121@cluster0.0ckpftt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Optional: Use Server API (recommended for compatibility with cloud MongoDB)
 client = MongoClient(MONGO_URI, server_api=ServerApi("1"))
